1607ddb8d59262c2ab501e97eb49c11bb06d90be
d4niel <olayajuandaniel8@gmail.com>
2022-11-07 17:44:24 -0500

    Docs

--- main.js
+++ main.js
@@ -35,16 +35,16 @@ class Snake{
    * @param {CSSStyleRule} color Color de la serpiente
    */
   constructor(canvas,color='#81D7FE'){
-    this.canvas = canvas
+    this.canvas = canvas // Canvas
     this.ctx = this.canvas.getContext('2d');
-    this.x = 10;
-    this.y = 10;
-    this.w = 10;
-    this.h = 10;
-    this.plus = 0
-    this.dir = 'R'
-    this.color = color;
-    this.apple = new Apple(this.canvas);
+    this.x = 10; // pos X
+    this.y = 10; // pos Y
+    this.w = 10; // ancho
+    this.h = 10; // alto
+    this.plus = 0 // Tamaño (cuando se colicione con una mazana aumenta)
+    this.dir = 'R' // direccion inicial
+    this.color = color; // Color de la culebra
+    this.apple = new Apple(this.canvas); // Instancia de apple
   }
   /**
    * Dibuja una serpiente en el elemento canvas
@@ -52,50 +52,53 @@ class Snake{
    * @returns {void}
    */
   draw(){
-    this.apple.draw();
-    console.log(this.y)
+    this.apple.draw(); // Se dibuja la manzana
     if((this.x >= this.apple.x && this.x <= this.apple.x + this.apple.w)
         && (this.y >= this.apple.y && this.y <= this.apple.y+this.apple.h)){
       this.plus +=10
       this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
       this.apple.x = Math.floor((Math.random() * (0 - Math.max(this.canvas.width,this.canvas.height))) + Math.min(this.canvas.width,this.canvas.height));
       this.apple.y = this.apple.x
-      console.log(this.apple.x, 'a')
     }
-    this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(this.x,this.y,this.w,this.h)
+    this.ctx.fillStyle = this.color; // se coloca el color
+    this.ctx.fillRect(this.x,this.y,this.w,this.h) // Se dibuja la culebra
   }
-  
+  /**
+   * Cambia la direccion de la culebrita
+   */
   changeDir(){
-    // console.log(this.snk[this.snk.length-1].posX)
     switch(this.dir){
-      case 'R':
+      case 'R': // Si es derecha
         this.x++;
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.x,this.y,this.w+this.plus,this.h)
         break;
-      case 'L':
+      case 'L': // Si es izq
         this.x--
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.x,this.y,this.w+this.plus,this.h);
         break;
-      case 'D':
+      case 'D': // Si es abajo
         this.y++;
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.x,this.y,this.w,this.h+this.plus)
         break
-      case 'U':
+      case 'U': // Si es arriba
         this.y--;
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.x,this.y,this.w,this.h+this.plus)
         break
     }
   }
+  /**
+   * Game Loop
+   * Recursive game loop
+   */
   update = () =>{
-    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-    this.draw()
-    this.changeDir()
-    requestAnimationFrame(this.update);
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height); // se borra el tablero
+    this.draw() // se dibuja la culebra
+    this.changeDir() // Se evalua si se ha cambiado de direccion
+    requestAnimationFrame(this.update); // Recursion
   }
 }
 
