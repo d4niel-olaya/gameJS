45efe2c0f633825f3c8504beea1c0914aec9283e
d4niel <olayajuandaniel8@gmail.com>
2022-11-07 17:38:33 -0500

    snak growing

--- main.js
+++ main.js
@@ -1,71 +1,5 @@
 const lienzo = document.getElementById('snake')
 
-class Snake{
-  /**
-   * @param {HTMLCanvasElement} canvas Elemento canvas
-   * @param {number} x Eje x de canvas 
-   * @param {number} y Eje y de canvas 
-   * @param {number} w Ancho de la serpiente
-   * @param {number} h Alto de la serpiente
-   * @param {CSSStyleRule} color Color de la serpiente
-   */
-  constructor(canvas,x=20,y=20,w=100,h=20,color='#81D7FE'){
-    this.canvas = canvas
-    this.ctx = this.canvas.getContext('2d');
-    this.x= x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.dir = 'R'
-    this.color = color;
-  }
-  /**
-   * Dibuja una serpiente en el elemento canvas
-   * @param {String} dir Dirección a donde la serpiente se va mover
-   * @returns {void}
-   */
-  draw(){
-    if((this.x+this.w) == this.canvas.width){
-      alert('se ha excedido');
-      this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
-    }
-    switch (this.dir){
-      case 'R':
-        this.h = 20;
-        this.w = 100;
-        this.x++;
-        break;
-      case 'L':
-        this.w = 100;
-        this.h = 20
-        this.x--;
-        break;
-      case 'D':
-        this.h = 100
-        this.w = 20
-        // this.w = this.w - 80
-        // this.w = 20
-        console.log(this.h)
-        // console.log(this.w)
-        this.y++
-        break;
-      case 'U':
-        this.h = 100
-        this.w = 20
-        this.y--
-        break;
-    }
-    this.ctx.fillStyle=this.color
-    this.ctx.fillRect(this.x, this.y, this.w, this.h);
-  }
-
-  update = () =>{
-    this.ctx.clearRect(this.x,this.y,this.w,this.h);
-    this.draw()
-    requestAnimationFrame(this.update);
-  }
-}
-
 class Apple{
   /**
    * 
@@ -76,7 +10,7 @@ class Apple{
    * @param {number} h Alto de la manzana
    * @param {CSSStyleRule} color Color de la manzana
    */
-  constructor(canvas,x = 150, y = 150, w = 20, h = 20, color = 'red'){
+  constructor(canvas,x = 140, y =140, w = 20, h = 20, color = 'red'){
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
     this.x = x;
@@ -95,10 +29,78 @@ class Apple{
     this.ctx.fillRect(this.x, this.y, this.w,this.h);
   }
 }
+class Snake{
+  /**
+   * @param {HTMLCanvasElement} canvas Elemento canvas
+   * @param {CSSStyleRule} color Color de la serpiente
+   */
+  constructor(canvas,color='#81D7FE'){
+    this.canvas = canvas
+    this.ctx = this.canvas.getContext('2d');
+    this.x = 10;
+    this.y = 10;
+    this.w = 10;
+    this.h = 10;
+    this.plus = 0
+    this.dir = 'R'
+    this.color = color;
+    this.apple = new Apple(this.canvas);
+  }
+  /**
+   * Dibuja una serpiente en el elemento canvas
+   * @param {String} dir Dirección a donde la serpiente se va mover
+   * @returns {void}
+   */
+  draw(){
+    this.apple.draw();
+    console.log(this.y)
+    if((this.x >= this.apple.x && this.x <= this.apple.x + this.apple.w)
+        && (this.y >= this.apple.y && this.y <= this.apple.y+this.apple.h)){
+      this.plus +=10
+      this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+      this.apple.x = Math.floor((Math.random() * (0 - Math.max(this.canvas.width,this.canvas.height))) + Math.min(this.canvas.width,this.canvas.height));
+      this.apple.y = this.apple.x
+      console.log(this.apple.x, 'a')
+    }
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.x,this.y,this.w,this.h)
+  }
+  
+  changeDir(){
+    // console.log(this.snk[this.snk.length-1].posX)
+    switch(this.dir){
+      case 'R':
+        this.x++;
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x,this.y,this.w+this.plus,this.h)
+        break;
+      case 'L':
+        this.x--
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x,this.y,this.w+this.plus,this.h);
+        break;
+      case 'D':
+        this.y++;
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x,this.y,this.w,this.h+this.plus)
+        break
+      case 'U':
+        this.y--;
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x,this.y,this.w,this.h+this.plus)
+        break
+    }
+  }
+  update = () =>{
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    this.draw()
+    this.changeDir()
+    requestAnimationFrame(this.update);
+  }
+}
+
 const snk = new Snake(lienzo);
-const appl = new Apple(lienzo);
 snk.draw();
-appl.draw()
 requestAnimationFrame(snk.update);
 document.addEventListener('keyup', (e) =>{
   switch(e.code){
